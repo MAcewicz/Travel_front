09b7da061f16cf715eaf91481ce51734f38323bb
MAcewicz <acewicz.m@gmail.com>
2019-10-13 21:53:46 +0100

    first commit

---
+++ src/main/java/com/kodilla/travelfront/TravelfrontApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.travelfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelfrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelfrontApplication.class, args);
+    }
+
+}
